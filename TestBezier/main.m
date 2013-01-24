bb9124cee2ba4ef809b6b0acd39fd98fea7e731d
ybrikeeg <kirby@geeteam.com>
2013-01-23 20:31:19 -0800

    Initial Commit

---
+++ TestBezier/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestBezier
+//
+//  Created by KirbyGee on 1/23/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
